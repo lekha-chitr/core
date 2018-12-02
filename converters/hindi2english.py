77ed91a8e3ea40676a7c76cf4cf19e23a071c840
lazzyguy <igenjarus@gmail.com>
2018-12-02 23:36:37 +0530

    implementing convertre

---
+++ converters/hindi2english.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import os
+
+from googletrans import Translator
+
+translator = Translator()
+
+translator.translate('안녕하세요.')
+
+# with open('test.txt', 'r') as f:
+    # text = f.read()
+    # text = text.replace('\n', '').split(' ')
+    # print(text)
+
+
